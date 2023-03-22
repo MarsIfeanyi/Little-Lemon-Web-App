a72309691e98bfbb6ad0bf53194be6ff6c0614c8
MarsIfeanyi <marschalice@gmail.com>
2023-03-22 11:15:27 +0100

    Add Fragment and HTML semantics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Fragment } from "react/cjs/react.production.min";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <Fragment className="App">
       <h1>HomePage</h1>
-    </div>
+
+      <header>
+        <nav></nav>
+      </header>
+
+      <main></main>
+
+      <footer></footer>
+    </Fragment>
   );
 }
 
