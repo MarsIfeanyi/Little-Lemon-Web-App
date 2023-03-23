ca830597536c096fb99a44cbecb90e6b819ea77b
MarsIfeanyi <marschalice@gmail.com>
2023-03-23 21:24:48 +0100

    Create Pages

---
+++ src/page/HomePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Main from "../components/Main";
+import Testimonial from "../components/Testimonial";
+import About from "../components/About";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+
+      <Main />
+
+      <Testimonial />
+
+      <About />
+    </div>
+  );
+};
+
+export default HomePage;
