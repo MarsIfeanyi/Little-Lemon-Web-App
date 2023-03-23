cf62a46398f94c2d20e417c66fcb7734898aa3c6
MarsIfeanyi <marschalice@gmail.com>
2023-03-23 21:20:40 +0100

    Add Images to assets folder

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,16 @@ import IntaLogo from "./InstaLogo.svg";
 import LinkedinLogo from "./LinkedinLogo.svg";
 import TwitterLogo from "./TwitterLogo.svg";
 import WhatsappLogo from "./WhatsappLogo.svg";
+import HeroImage from "./HeroImage.jpg";
+import Greeksalad from "./Greeksalad.jpg";
+import Lemondessert from "./Lemondessert.jpg";
+import Bruchetta from "./Bruchetta.svg";
+import Noodles from "./Noodles.png";
+import Image1 from "./image.png";
+import Image2 from "./image2.png";
+import Image3 from "./image3.png";
+import Image4 from "./image4.png";
+import Star from "./star.png";
 
 export {
   littleLogo,
@@ -12,4 +22,14 @@ export {
   LinkedinLogo,
   TwitterLogo,
   WhatsappLogo,
+  HeroImage,
+  Greeksalad,
+  Lemondessert,
+  Bruchetta,
+  Noodles,
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Star,
 };
