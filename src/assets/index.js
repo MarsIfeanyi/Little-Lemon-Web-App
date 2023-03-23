37e4328abffbae5a82c252bc58dbd8bcdfdfaf76
MarsIfeanyi <marschalice@gmail.com>
2023-03-23 10:29:16 +0100

    Import and Export logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,15 @@
 import littleLogo from "./littleLogo .svg";
 import FacebookLogo from "./FacebookLogo.svg";
+import IntaLogo from "./InstaLogo.svg";
+import LinkedinLogo from "./LinkedinLogo.svg";
+import TwitterLogo from "./TwitterLogo.svg";
+import WhatsappLogo from "./WhatsappLogo.svg";
 
-export { littleLogo, FacebookLogo };
+export {
+  littleLogo,
+  FacebookLogo,
+  IntaLogo,
+  LinkedinLogo,
+  TwitterLogo,
+  WhatsappLogo,
+};
