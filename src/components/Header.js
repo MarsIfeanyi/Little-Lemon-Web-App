bb47ead7a263fde89abf6203f77bedf6ecb02f43
MarsIfeanyi <marschalice@gmail.com>
2023-03-23 21:22:46 +0100

    Update component and Add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,13 @@ import { littleLogo } from "../assets";
 const Header = () => {
   return (
     <Fragment>
-      <div className="flex flex-col md:flex-row justify-between">
+      <div className="flex flex-col md:flex-row justify-between mt-10  border-b-2 shadow-lg fixed top-0 left-0 right-0 ">
         <header>
-          <img src={littleLogo} alt="littleLemonLogo" className="w-52" />
+          <img
+            src={littleLogo}
+            alt="littleLemonLogo"
+            className="w-52  ml-20 mb-4"
+          />
         </header>
 
         <Nav />
