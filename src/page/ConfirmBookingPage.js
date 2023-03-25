1a9a0d32204d81333c08996dea2ee1bf2bb5083f
MarsIfeanyi <marschalice@gmail.com>
2023-03-25 21:28:08 +0100

    Create Confirm Booking Form page

---
+++ src/page/ConfirmBookingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Fragment } from "react";
+import ConfirmBooking from "../components/ConfirmBooking";
+
+export const ConfirmBookingPage = () => {
+  return (
+    <Fragment>
+      <ConfirmBooking />
+    </Fragment>
+  );
+};
