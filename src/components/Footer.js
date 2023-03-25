13e4fcbf8dfb537991284bf3a620c8fe91519ddf
MarsIfeanyi <marschalice@gmail.com>
2023-03-25 21:25:37 +0100

    Add styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ import {
 const Footer = () => {
   return (
     <Fragment>
-      <div className="mt-14  flex flex-col md:flex-row justify-between mb-6 ">
+      <div className="mt-14  flex flex-col md:flex-row justify-between mb-6 ml-16 md:ml-0">
         <header>
           <img src={littleLogo} alt="littleLemonLogo" className="ml-20 w-52 " />
         </header>
@@ -39,15 +39,15 @@ const Footer = () => {
           </ul>
         </nav>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-8 md:mt-0">
           <h2 className="text-2xl">Contact </h2>
-          <p className="mt-4">
+          <p className="mt-4 text-xl">
             #10 Chicago street <br /> littlemon@gmail.com <br />
             +1205908930383
           </p>
         </div>
 
-        <div className="flex flex-col space-y-4 mr-20 ">
+        <div className="flex flex-col space-y-4 mr-20 mt-6 md:mt-0 ">
           <img src={FacebookLogo} alt="facebookLogo" className="w-10" />
           {/* <img src={IntaLogo} alt="intagramLogo" className="w-10" /> */}
           <img src={TwitterLogo} alt="twitterLogo" className="w-10" />
